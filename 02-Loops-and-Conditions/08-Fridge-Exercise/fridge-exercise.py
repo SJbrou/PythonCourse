2ad09a09538584b658be1d5b8dac6ad94af2a2bf
SJbrou <155914518+SJbrou@users.noreply.github.com>
2024-09-28 19:41:57 +0200

    some python exercises

--- 02-Loops-and-Conditions/08-Fridge-Exercise/fridge-exercise.py
+++ 02-Loops-and-Conditions/08-Fridge-Exercise/fridge-exercise.py
@@ -10,3 +10,36 @@ Get the user to enter a temperature in celsius.
 > 6: Print "Fridge broken"
 
 """
+
+
+# Version 1
+"""
+temp = float(input("temperatrure in celcius: "))
+
+if temp < 0:
+    print("Fridge too cold")
+elif temp >= 0 and temp < 4:
+    print("Fridge OK")
+elif temp >= 4 and temp < 6:
+    print("Fridge too warm")
+elif temp > 6:
+    print("Fridge broken")
+"""
+
+# Version 2
+STATUS_BROKEN = "Fridge is broken."
+STATUS_OK = "Fridge OK."
+STATUS_WARM = "Fridge is too warm."
+STATUS_COLD = "Fridge is too cold."
+status = STATUS_BROKEN
+
+temp = float(input("Enter the fridge temperature: "))
+
+if temp < 0:
+    status = STATUS_COLD
+elif temp <= 4:
+    status = STATUS_OK
+elif temp <= 6:
+    status = STATUS_WARM
+
+print(status)
--- 02-Loops-and-Conditions/13-Indentation/indentation.py
+++ 02-Loops-and-Conditions/13-Indentation/indentation.py
@@ -4,5 +4,3 @@ for i in range(3):
 
     if i == 2:
         print("i is 2")
-
-
